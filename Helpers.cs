0f32261945cea2d171c9f444c5452176ae52241b
Ville M. Vainio <ville.vainio@basware.com>
2020-09-16 11:01:27 +0300

    helpers

--- Helpers.cs
+++ Helpers.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace AssyCheck
 {
